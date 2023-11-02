35200f55830bbff46105b0738c474481decb354d
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-02 09:52:44 +0200

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+  description = "The type of EC2 instance to launch."
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance."
+}

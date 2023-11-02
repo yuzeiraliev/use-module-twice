7484742cd61d5f7e649ae94428727d07c9e64428
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-02 09:53:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,11 +31,4 @@ module "module_us_west_2" {
   }
 }
 
-output "us_east_1_instance_public_ip" {
-  value = module.module_us_east_1.example.public_ip
-}
-
-output "us_west_2_instance_public_ip" {
-  value = module.module_us_west_2.example.public_ip
-}
 
